cef35d31c96ce4f8791a4a5678aac9d880d44216
moko <ljun925@naver.com>
2024-06-08 19:33:32 +0900

    study: Lists - odd, even, first, last

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 export default function Home() {
   return (
     <main className="bg-gradient-to-bl from-green-200 via-purple-200 to-blue-200 h-screen sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100  xl:bg-orange-100 flex items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full ring ring-transparent transition-shadow max-w-screen-sm flex flex-col gap-2 md:flex-row has-[:invalid]:ring-red-100 has-[:invalid]:ring">
-        <input
-          className="w-full rounded-full py-3 bg-gray-200 pl-5   ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow-sm invalid:focus:ring-red-100 peer"
-          type="text"
-          required
-          placeholder="이메일"
-        />
-
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          올바른 이메일 형식이 아닙니다.
-        </span>
-
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium  md:px-10">
-          로그인
-        </button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full  max-w-screen-sm flex flex-col gap-3">
+        {['moko', 'me', 'you', 'yourself'].map((person, index) => (
+          // <div
+          //   key={index}
+          //   className="flex items-center gap-5 odd:bg-gray-100 p-2.5 rounded-xl even:bg-cyan-100"
+          // >
+          <div
+            key={index}
+            className="flex items-center gap-5 border-b-2 pb-5 last:border-0 last:pb-0"
+          >
+            <div className="size-7 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-5 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
