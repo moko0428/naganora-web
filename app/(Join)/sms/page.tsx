83426379d7d30927b3eb370840b8e11705e129d7
moko <ljun925@naver.com>
2024-06-17 17:31:59 +0900

    feat: sms validation

--- app/(Join)/sms/page.tsx
+++ app/(Join)/sms/page.tsx
@@ -1,30 +1,42 @@
-import FormInput from '@/components/input';
-import FormBtn from '@/components/button';
-import SocialLogin from '@/components/social-login';
+'use client';
+import Input from '@/components/input';
+import Button from '@/components/button';
+import { useFormState } from 'react-dom';
+import { smsLogin } from './actions';
+
+const initialState = {
+  token: false,
+  error: undefined,
+};
 
 export default function SMSLogin() {
+  const [state, dispatch] = useFormState(smsLogin, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl ">전화번호로 로그인</h1>
         <h2 className="text-xl">나가노라에 오신것을 환영합니다.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput
-          name="phoneNumber"
-          type="number"
-          placeholder="전화번호"
-          required
-          errors={[]}
-        />{' '}
-        <FormInput
-          name="verifyCode"
-          type="number"
-          placeholder="인증코드"
-          required
-          errors={[]}
-        />
-        <FormBtn text="로그인" />
+      <form action={dispatch} className="flex flex-col gap-3">
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="인증코드"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="전화번호"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? '인증 코드' : '인증 코드 보내기'} />
       </form>
     </div>
   );
