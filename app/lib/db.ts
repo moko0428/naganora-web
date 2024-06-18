5c373e17e146b908dfe4bb7b623f7f0bb45b4f2a
moko <ljun925@naver.com>
2024-06-18 23:31:22 +0900

    feat: smstoken model 생성

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -3,12 +3,12 @@ import { PrismaClient } from '@prisma/client';
 const db = new PrismaClient();
 
 async function test() {
-  const user = await db.user.create({
-    data: {
-      nickname: 'test',
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
     },
   });
-  console.log(user);
+  console.log(token);
 }
 test();
 export default db;
