b2a5e4f355764dfdcf5f7f5d001ac03aef42f6f3
moko <ljun925@naver.com>
2024-06-19 20:09:57 +0900

    feat: session을 이용하여  log out, notFound()를 사용하여 session 없는 사용자 침입 막기

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,37 @@
-export default function Profile() {
-  return <h1>profile</h1>;
+import { notFound, redirect } from 'next/navigation';
+import db from '../lib/db';
+import getSession from '../lib/session';
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    'use server';
+    const session = await getSession();
+    await session.destroy();
+    redirect('/');
+  };
+  return (
+    <div>
+      <h1>{user?.nickname}</h1>
+      {/* 원래라면 버튼에 onClick을 넣어서 redirect하는게 맞지만 이럴 경우엔 컴포넌트가 client 컴포넌트가 되므로 server 컴포넌트에선 form의 action에 넣어서 관리하면 form이 submit해주기 때문에 가능하다*/}
+      <form action={logOut}>
+        <button>Log out</button>
+      </form>
+    </div>
+  );
 }
