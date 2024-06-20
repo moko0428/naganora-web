a7351601c13c1f641668c4e0f288e748bee45ad5
moko <ljun925@naver.com>
2024-06-21 02:24:09 +0900

    feat: 깃헙으로 로그인

--- app/(Join)/github/complete/route.ts
+++ app/(Join)/github/complete/route.ts
@@ -1,4 +1,6 @@
-import { notFound } from 'next/navigation';
+import db from '@/app/lib/db';
+import getSession from '@/app/lib/session';
+import { notFound, redirect } from 'next/navigation';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
@@ -12,18 +14,52 @@ export async function GET(request: NextRequest) {
     client_secret: process.env.GITHUB_CLIENT_SECRET!,
     code,
   }).toString();
-  const accessTokenURL = `'https://github.com/login/oauth/access_token'?${accessTokenParams}`;
+  const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
   const accessTokenResponse = await fetch(accessTokenURL, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
     },
   });
-  const accessTokenData = await accessTokenResponse.json();
-  if ('error' in accessTokenData) {
+  const { error, access_token } = await accessTokenResponse.json();
+  if (error) {
     return new Response(null, {
       status: 400,
     });
   }
-  return Response.json({ accessTokenData });
+  const userProfileResponse = await fetch('https://api.github.com/user', {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+    cache: 'no-cache',
+  });
+  const { id, avatar_url, login } = await userProfileResponse.json();
+  const user = await db.user.findUnique({
+    where: {
+      github_id: id + '',
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (user) {
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+    return redirect('/profile');
+  }
+  const newUser = await db.user.create({
+    data: {
+      nickname: `${login}-gh`,
+      github_id: id + '',
+      avatar: avatar_url,
+    },
+    select: {
+      id: true,
+    },
+  });
+  const session = await getSession();
+  session.id = newUser.id;
+  await session.save();
+  return redirect('/profile');
 }
