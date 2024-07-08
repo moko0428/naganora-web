28504885b38f5be99c9370869308f6c5637e60ee
moko <ljun925@naver.com>
2024-07-08 22:43:09 +0900

    product 컴포넌트

--- app/(Tabs)/products/page.tsx
+++ app/(Tabs)/products/page.tsx
@@ -1,12 +1,26 @@
+import db from '@/app/lib/db';
+import ListProduct from '@/components/list-product';
+
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+  return products;
 }
 
 export default async function Products() {
   const products = await getProducts();
   return (
-    <div>
-      <h1>Products</h1>
+    <div className="p-5 flex flex-col">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
