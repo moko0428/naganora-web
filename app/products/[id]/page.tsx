3ba07444c302563958130894ea788a7738f86928
moko <ljun925@naver.com>
2024-07-10 17:14:03 +0900

    product detail

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,86 @@
-async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+import db from '@/app/lib/db';
+import getSession from '@/app/lib/session';
+import { formatToWon } from '@/app/lib/utils';
+import { UserIcon } from '@heroicons/react/16/solid';
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
+  return false;
 }
-export default async function ProductsDetail({
-  params: { id },
+
+async function getProduct(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          nickname: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return product;
+}
+export default async function ProductDetail({
+  params,
 }: {
   params: { id: string };
 }) {
-  const product = await getProduct();
-  return <h1>detail{id}</h1>;
+  const id = Number(params.id);
+  if (isNaN(id)) {
+    return notFound();
+  }
+  const product = await getProduct(id);
+  if (!product) {
+    return notFound();
+  }
+  const isOwner = await getIsOwner(product.userId);
+  return (
+    <div>
+      <div className="relative aspect-square">
+        <Image fill src={product.photo} alt={product.title} />
+      </div>
+      <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+        <div className="size-10 rounded-full">
+          {product.user.avatar !== null ? (
+            <Image src={product.user.avatar} width={40} height={40} />
+          ) : (
+            <UserIcon />
+          )}
+        </div>
+        <div>
+          <h3>{product.user.nickname}</h3>
+        </div>
+      </div>
+      <div className="p-5 ">
+        <h1 className="text-2xl font-semibold">{product.title}</h1>
+        <p>{product.description}</p>
+      </div>
+      <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 justify-between items-center">
+        <span className="font-semibold text-xl">
+          {formatToWon(product.price)}원
+        </span>
+        {isOwner ? (
+          <button className="bg-red-500 px-5 py-2 rounded-md text-white font-semibold">
+            Delete product
+          </button>
+        ) : null}
+        <Link
+          className="bg-main px-5 py-2 rounded-md text-white font-semibold"
+          href={``}
+        >
+          채팅하기
+        </Link>
+      </div>
+    </div>
+  );
 }
